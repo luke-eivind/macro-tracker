e6a68b8f181dee29500817bb795c95962e049a91
Logan <Logan.fr33man@gmail.com>
2022-03-31 13:38:44 -0500

    drawer meme

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,6 +5,73 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import $ from 'jquery';
+import ListItemText from '@mui/material/ListItemText';
+import {
+  Drawer,
+  Toolbar,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+} from '@mui/material';
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiAppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
 
 export default function LandingPage(props) {
   const [calories, setCalories] = useState('');
@@ -26,7 +93,6 @@ export default function LandingPage(props) {
   useEffect(() => {
     createPie('.pieID.legend', '.pieID.pie');
   });
-
   function sliceSize(dataNum, dataTotal) {
     return (dataNum / dataTotal) * 360;
   }
@@ -101,40 +167,143 @@ export default function LandingPage(props) {
   }
 
   const changeCaloriesHandler = (event) => {
-    event.target.value = Number.isInteger(Number(event.target.value))
-      ? event.target.value
-      : 0;
     setCalories(event.target.value);
     setInputText(event.target.value);
   };
 
   const changeCarbsHandler = (event) => {
-    event.target.value = Number.isInteger(Number(event.target.value))
-      ? event.target.value
-      : 0;
     setCarbs(event.target.value);
     setInputText(event.target.value);
   };
 
   const changeFatHandler = (event) => {
-    event.target.value = Number.isInteger(Number(event.target.value))
-      ? event.target.value
-      : 0;
     setFat(event.target.value);
     setInputText(event.target.value);
   };
 
   const changeProteinHandler = (event) => {
-    event.target.value = Number.isInteger(Number(event.target.value))
-      ? event.target.value
-      : 0;
     setProtein(event.target.value);
     setInputText(event.target.value);
+  };
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
   };
 
   return (
     <div>
-      <h1>Macro Tracker</h1>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant='persistent'
+        anchor='left'
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <AppBar position='fixed' open={open}>
+        <Toolbar>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            edge='start'
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' noWrap component='div'>
+            Macro Tracker
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant='persistent'
+        anchor='left'
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
       <div class='card'>
         <Box
           component='form'
@@ -147,6 +316,17 @@ export default function LandingPage(props) {
           <TextField
             id='calories-input'
             label='Calories'
+            type='number'
+            sx={{
+              '& .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'white',
+                text: 'white',
+              },
+              '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'white',
+                text: 'white',
+              },
+            }}
             style={{ margin: '30px', position: 'relative' }}
             variant='outlined'
             onChange={changeCaloriesHandler}
@@ -154,6 +334,17 @@ export default function LandingPage(props) {
           <TextField
             id='carbs-input'
             label='Carbs'
+            type='number'
+            sx={{
+              '& .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'white',
+                text: 'white',
+              },
+              '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'white',
+                text: 'white',
+              },
+            }}
             style={{ margin: '30px', position: 'relative' }}
             variant='outlined'
             onChange={changeCarbsHandler}
@@ -161,6 +352,17 @@ export default function LandingPage(props) {
           <TextField
             id='fat-input'
             label='Fat'
+            type='number'
+            sx={{
+              '& .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'white',
+                text: 'white',
+              },
+              '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'white',
+                text: 'white',
+              },
+            }}
             style={{ margin: '30px', position: 'relative' }}
             variant='outlined'
             onChange={changeFatHandler}
@@ -168,6 +370,17 @@ export default function LandingPage(props) {
           <TextField
             id='protein-input'
             label='Protein'
+            type='number'
+            sx={{
+              '& .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'white',
+                text: 'white',
+              },
+              '& .Mui-focused .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'white',
+                text: 'white',
+              },
+            }}
             style={{ margin: '30px', position: 'relative' }}
             variant='outlined'
             onChange={changeProteinHandler}
